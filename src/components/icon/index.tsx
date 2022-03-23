f249836916b66a14d7fd6ef098d81f77ace9dcd9
DoneDeal0 <ap.lanoe@outlook.com>
2022-03-23 19:52:04 +0100

    fix: icon file

--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,14 +1,16 @@
 import React, { memo } from "react";
-import { Icons, AllIcons } from "../../models/Icon";
+import filters from "assets/svg/filters.svg";
+
+//import { Icons, AllIcons } from "../../models/Icon";
 
 interface IconProps {
-  icon: Icons;
+  icon: any; //Icons;
   size?: number;
   color?: string;
 }
 
 const Icon = memo(({ icon, size = 32, color }: IconProps) => {
-  const Container = AllIcons[icon];
+  const Container = filters; // AllIcons[icon];
   return <Container icon={icon} width={size} height={size} color={color} />;
 });
 
